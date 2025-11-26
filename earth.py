7c24b123d93fd7492ee9428c642234e5648128d7
Nasadude <mike@technating.io>
2025-11-26 13:06:40 -0600

    Added Rk4 method and numericall_integration

--- earth.py
+++ earth.py
@@ -87,8 +87,95 @@ euler_method(r, v, accn, dt)
 print(r)
 '''
 
-# Apply the Euler Integration on the given conditions
-euler_method(r, v, accn, dt)
+
+# RK4 Integration
+def rk4_method(r, v, accn, dt):
+    """
+    Equations for RK4 method
+    --------------------------
+    ODE for Position
+    --> dr/dt = v
+    --> r_new = r_old + dt/6(k1r + 2*k2r + 2*k3r + k4r)
+
+    ODE for Velocity
+    --> dv/dt = a
+    --> v_new = v_old + dt/6(k1v + 2*k2v + 2*k3v + k4v)
+
+    Method to Calculate steps
+    ---------------
+    Step 1:– 0
+    k1v = accn(r[i-1])
+    k1r = v[i-1]
+
+    Step 2:– dt/2 using step 1
+    k2v = accn(r[i-1] + k1r * dt/2)
+    k2r = v[i-1] + k1v * dt/2
+
+    Step 3:– dt/2 using step 2
+    k3v = accn(r[i-1] + k2r * dt/2)
+    k3r = v[i-1] + k2v * dt/2
+
+    Step 4:– dt using step 3
+    k4v = accn(r[i-1] + k3r * dt)
+    k4r = v[i-1] + k3v * dt
+
+    Parameters
+    ----------
+    r: empty array for position of size t
+    v: empty array for velocity of size t
+    a: function to calculate accn at given position
+    dt: time step of simulation
+
+    This function will update the empty arrays for r and v with the simulated data
+    """
+    for i in range(1, len(t)):
+        #Step 1:– 0
+        k1v = accn(r[i-1])
+        k1r = v[i-1]
+
+        #Step 2:– dt/2 using step 1
+        k2v = accn(r[i-1] + k1r * dt/2)
+        k2r = v[i-1] + k1v * dt/2
+
+        #Step 3:– dt/2 using step 2
+        k3v = accn(r[i-1] + k2r * dt/2)
+        k3r = v[i-1] + k2v * dt/2
+
+        #Step 4:– dt using step 3
+        k4v = accn(r[i-1] + k3r * dt)
+        k4r = v[i-1] + k3v * dt 
+
+        # Update the r and v
+        v[i] = v[i-1] + dt/6 * (k1v + 2*k2v + 2*k3v + k4v)
+        r[i] = r[i-1] + dt/6 * (k1r + 2*k2r + 2*k3r + k4r)
+
+
+def numerical_integration(r, v, accn, dt, method='euler'):
+    """
+    This function will apply the numerical_integration based on the method choosen
+    If the method is euler or rk4, the respective method will be implemented
+    Else it will raise an Exception
+
+    Parameters
+    ----------
+    r: empty array for position of size t
+    v: empty array for velocity of size t
+    a: func to calculate the accn at give position
+    dt: time step for the simulation
+    method: either rk4 or euler
+    """
+
+    if method.lower()=='euler':
+        euler_method(r, v, accn, dt)
+
+    elif method.lower()=='rk4':
+        rk4_method(r, v, accn, dt)
+
+    else:
+        raise Exception(f'You can either choose "euler" or "rk4". Your current input for method is:- {method}')
+
+# Implementation of numerical_integration 
+numerical_integration(r, v, accn, dt, method=("rk4"))
 
 # Find the point at which Earth is at its Aphelion
 sizes = np.array([np.linalg.norm(position) for position in r])
